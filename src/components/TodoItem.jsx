7e486cbeaf90d19e2f9731c2c0cea431aee310d0
mihatormoz <mcohotnik21@gmail.com>
2022-07-25 19:02:05 +0300

    Added a checkbox, no localstorage

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const TodoItem = (props) => {
+    const [isChecked, setChecked] = useState(false)
     if (props.todo.title.length < 1) {
         return false
     }
+    const checkedTodo = () => {
+        if (isChecked) {
+            setChecked(false)
+        } else {
+            setChecked(true)
+        }
+    }
     return (
-        <div>
-            <h1>{props.todo.id}. {props.todo.title}</h1>
+        <div style={{display: 'flex', justifyContent: 'space-between'}}>
+            <input type="checkbox" onClick={() => checkedTodo()}/>
+            {isChecked
+            ? <h1 style={{textDecoration: 'line-through'}}>{props.todo.id}. {props.todo.title}</h1>
+            : <h1>{props.todo.id}. {props.todo.title}</h1>
+            }
             <button onClick={() => props.remove(props.todo)}>Remove</button>
         </div>
     )
